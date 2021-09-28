2d60107933f2f75b5daa66b6e2612673099c35d2
Thomas Nguyen <70709101+ThomasVuNguyen@users.noreply.github.com>
2021-09-28 16:46:49 -0500

    Add files via upload

---
+++ thomas93/keymaps/default/keymap.c
@@ -0,0 +1,10 @@
+#include "kb.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	KEYMAP(
+		KC_WH_U, KC_BTN3,KC_WH_D, 
+		KC_BTN1, KC_MS_U,  KC_BTN2, 
+		KC_MS_L, KC_MS_D, KC_MS_R),
+};
+
